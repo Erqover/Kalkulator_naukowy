e02667e8933d5ee2e31aeab55273bfceef9e6c05
matiw <erqover.armcrode@gmail.com>
2023-04-24 21:08:09 +0200

    Dodaj pliki projektów.

---
+++ Kalkulator_naukowy/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace armcrode
+{
+    class Kalkulator
+    {
+        static void Main(string[] args)
+        {
+            bool program = true;
+            int case_switch = 0;
+            string temp__case_switch ="";
+            while (program)
+            {
+                if (temp__case_switch == null || temp__case_switch == "")
+                {
+                    temp__case_switch = Console.ReadLine();
+                }
+
+                switch(case_switch)
+                { 
+                    default:
+                        Console.WriteLine("nie podałeś żadnej opcji");
+                    break;
+
+            }
+            }
+        }
+    }
+}
