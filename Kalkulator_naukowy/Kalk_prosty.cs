8e49cb1f175a36bfad5d6899e4d74a11beef55e6
Mateusz Waręda <erqover.armcrode@gmail.com>
2023-04-28 23:20:04 +0200

    v0.2 - stworzenie kalkulatora prostego -- obsługa dodawania, odejmowania, dzielenia, mnożenia -- poprawa błędów -- poprawa interfejsu użytkownika

---
+++ Kalkulator_naukowy/Kalk_prosty.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kalkulator_naukowy
+{
+    internal class Kalk_prosty
+    {
+        private double _wynik = 0;
+        public Kalk_prosty()
+        {
+
+        }
+        ~Kalk_prosty()
+        {
+
+        }
+
+        public double Wynik { get => _wynik; set => _wynik = value; }
+
+        public void oblicz(string dzia) //działanie
+        {
+            dzia += " ";
+            string numb = "";
+            var list_ofn = new List<string>();
+            char sight = ' ';
+            for (int i=0;i<dzia.Length;i++)
+            {
+                if (dzia[i] == ' ' && numb != "")
+                {
+                    list_ofn.Add(numb);
+                    numb = "";
+                }
+                else if (dzia[i] == '+'
+                    || dzia[i] == '-'
+                    || dzia[i] == '*'
+                    || dzia[i] == '/')
+                {
+                    sight = dzia[i];
+                }
+                else if (dzia[i] == '.' || dzia[i] == ',')
+                {
+                    numb += ',';
+                }
+                else
+                {
+                    numb += dzia[i];
+                }
+            }
+            switch(sight)
+            {
+                default:
+                    return;
+                case '+':
+                    Wynik = Convert.ToDouble(list_ofn[0]) + Convert.ToDouble(list_ofn[1]);
+                    break;
+                case '-':
+                    Wynik = Convert.ToDouble(list_ofn[0]) - Convert.ToDouble(list_ofn[1]);
+                    break;
+                case '*':
+                    Wynik = Convert.ToDouble(list_ofn[0]) * Convert.ToDouble(list_ofn[1]);
+                    break;
+                case '/':
+                    Wynik = Convert.ToDouble(list_ofn[0]) / Convert.ToDouble(list_ofn[1]);
+                    break;
+            }
+            return;
+        }
+    }
+}
--- Kalkulator_naukowy/Program.cs
+++ Kalkulator_naukowy/Program.cs
@@ -1,27 +1,72 @@
-﻿namespace armcrode
+﻿using System;
+
+namespace Kalkulator_naukowy
 {
     class Kalkulator
     {
         static void Main(string[] args)
         {
             bool program = true;
-            int case_switch = 0;
-            string temp__case_switch ="";
+            string case_switch ="";
             while (program)
             {
-                if (temp__case_switch == null || temp__case_switch == "")
+                //Console.ReadKey();
+                Console.Clear();
+                case_switch = "";
+                Console.WriteLine("podaj tryb kalkulatora \n" +
+                    "1. tryb prosty \n" +
+                    "2. tryb naukowy \n" +
+                    "3. tryb programistyczny");
+                if (case_switch == null || case_switch == "")
                 {
-                    temp__case_switch = Console.ReadLine();
+                    case_switch = Console.ReadLine();
                 }
-
                 switch(case_switch)
                 { 
                     default:
-                        Console.WriteLine("nie podałeś żadnej opcji");
-                    break;
+                        Console.WriteLine("podałeś błędną opcję");
+                        break;
 
+                    case "1":
+                        prosty();
+                        break;
+                    case "2":
+                        Console.WriteLine("wybrałeś tryb naukowy");
+                        break;
+                    case "3":
+                        Console.WriteLine("wybrałeś tryb programistyczny");
+                        break;
+                }
             }
+        }
+
+        static void prosty()
+        {
+            Console.Clear();
+            string case_s = "";
+            Kalk_prosty obj = new Kalk_prosty();
+            while (true)
+            {
+                Console.WriteLine("napisz diałanie \n" +
+                    "q -  wyjście \n" +
+                    "enter - wyświetl wynik");
+                case_s = Console.ReadLine();
+                Console.Clear();
+                switch (case_s)
+                {
+                    default:
+                        obj.oblicz(case_s);
+                        break;
+                    case "":
+                        Console.WriteLine("wynik to: " + obj.Wynik);
+                        Console.WriteLine("______________________");
+                        Console.WriteLine();
+                        break;
+                    case "q":
+                        return;
+                }
             }
+
         }
     }
 }
